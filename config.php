38ef88b1a3b9cfab83e710e471baf1bd15d14250
aligatorr89 <rok.spacapan89@gmail.com>
2016-06-24 01:59:24 +0200

    Update config.php

--- config.php
+++ config.php
@@ -10,8 +10,10 @@ define('UTIL', 'util/');
 define('DB_TYPE', 'mysql'); 
 define('DB_USER', 'root');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'mydb');
-define('DB_PASSWORD', '123');
-define('DB_LOGIN_TABLE', 'mvc');
+//name of your DB
+define('DB_NAME', 'yourDb');
+define('DB_PASSWORD', 'yourPassword');
+// tables you must have. Rename them on your will.
+define('DB_LOGIN_TABLE', 'users');
 define('DB_NOTES_TABLE', 'notes');
 define('DB_SHOUT_TABLE', 'shouts');
